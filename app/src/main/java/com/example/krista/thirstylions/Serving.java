a543af7034ab6c9266a1466c32e4b54701cc53a1
Aldrich <aldrichfung@gmail.com>
2015-04-21 19:09:36 -0400

    Base Serving class created

--- app/src/main/java/com/example/krista/thirstylions/Serving.java
+++ app/src/main/java/com/example/krista/thirstylions/Serving.java
@@ -3,5 +3,36 @@ package com.example.krista.thirstylions;
 /**
  * Created by Aldrich on 4/21/2015.
  */
-public class Serving {
+public class Serving extends Thread{
+
+    private Player player;
+    private Customer customer;
+    private Tap tap;
+
+    public Serving(Player p, Customer c, Tap t){
+        this.player = p;
+        this.customer = c;
+        this.tap = t;
+
+        lockResources();
+    }
+
+    public void lockResources(){
+        synchronized(tap){
+            synchronized (customer){
+                try{
+                    this.sleep(5000);
+                }
+                catch (InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+
+                increaseScore();
+            }
+        }
+    }
+
+    public void increaseScore(){
+        player.setScore(player.getScore() + 1);
+    }
 }
